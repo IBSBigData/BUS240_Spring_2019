f6dbe0f26ee59f51dd59edabeb8a7fccdc074c64
rhcarver <rhcarver@users.noreply.github.com>
2018-10-11 10:18:46 -0400

    Create BUS240_packages.R

---
+++ BUS240_packages.R
@@ -0,0 +1,17 @@
+# some packages for BUS240 Session 06 & 07
+install.packages("tidyverse")
+install.packages("gapminder")
+install.packages("lubridate")
+install.packages("PerformanceAnalytics")
+install.packages("GGally")
+install.packages("rvest")
+install.packages("geomnet")
+install.packages("maps")
+install.packages("car")
+install.packages("animation")
+install.packages("plotly")
+
+############################################################
+#  One package used in DataCamp is called "gganimate", which installed
+# easily in older versions of R, but quite complicated on current version of R
+# we'll use plotly instead.
